c0ade4e8e6595b229fe501b9f63ebcb495de783c
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-02-24 19:13:45 +0530

    Takes username as input Prints all the files ever written.

--- index.go
+++ index.go
@@ -7,19 +7,42 @@ import (
   "github.com/PuerkitoBio/goquery"
 )
 
+/*
+@param Url of the repo
+About: Sends retrieval functions for fetching files
+*/
+
+func RepoRet(uri string){
+  doc, err := goquery.NewDocument(uri)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  doc.Find("tr.js-navigation-item").Each(func(i int, s *goquery.Selection) {
+    s.Find("td.content").Each(func(j int, sel *goquery.Selection){
+      fmt.Println(sel.Find("a").Text())
+    })
+  })
+}
+
 func ExampleScrape() {
-  doc, err := goquery.NewDocument("https://github.com/shubhodeep9?tab=repositories") 
+  baseuri := "https://github.com"
+  var user string
+  fmt.Println("Enter username")
+  fmt.Scanf("%s",&user)
+  doc, err := goquery.NewDocument("https://github.com/"+user+"?tab=repositories") 
   if err != nil {
     log.Fatal(err)
   }
 
   doc.Find("h3.repo-list-name").Each(func(i int, s *goquery.Selection) {
-    band := s.Find("a")
-    fmt.Println(band.Text())
+    val, _ := s.Find("a").Attr("href")
+    RepoRet(baseuri+val)
   })
-  //fmt.Println(doc)
+  
 }
 
 func main() {
   ExampleScrape()
+  //RepoRet("https://github.com/shubhodeep9/Gitlines")
 }
